c2e5ff4face68ff6d62c5bc13ae32d3b134460b7
Shalom Prinz <shalom.prinz@gmail.com>
2022-09-28 00:37:22 +0300

    navbar is now responsive and looks good

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,78 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Image from 'react-bootstrap/Image'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+import "./NavBar.css";
 
-const NavBar = () => {
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
-    <nav className="navbar navbar-expand-lg bg-primary p-3">
-      <Image height="100px" src="./src/res/cl_logo.png" title="Champions League" />
-      <Link className="navbar-brand mx-5" to="/"> Home </Link>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink to="/" className="nav-logo">
+            <Image 
+              height="90px"
+              className="p-2 m-3 filter-invert" 
+              src="./src/res/cl_logo.png" 
+              title="Champions League" 
+            />
+            Fantasy
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/blog"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            {click && <FontAwesomeIcon icon={faTimes} />}
+            {!click && <FontAwesomeIcon icon={faBars} />}
+          </div>
+        </div>
+      </nav>
+    </>
   );
-};
+}
 
 export default NavBar;
