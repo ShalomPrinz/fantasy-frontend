51ac183ba75e95cdc883229b7e5a0529cbc28ac5
Shalom Prinz <shalom.prinz@gmail.com>
2022-09-28 09:37:33 +0300

    finish navbar ui

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,77 +1,58 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Image from "react-bootstrap/Image";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
 import "./NavBar.css";
 
 function NavBar() {
-  const [click, setClick] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
-  const handleClick = () => setClick(!click);
   return (
-    <>
-      <nav className="navbar">
-        <div className="nav-container">
-          <NavLink to="/" className="nav-logo">
-            <Image 
-              height="90px"
-              className="p-2 m-3 filter-invert" 
-              src="./src/res/cl_logo.png" 
-              title="Champions League" 
-            />
-            Fantasy
+    <Navbar className="navbar" expand="lg" expanded={expanded}>
+      <NavLink className="navbar-brand" to="/">
+        <Image 
+          height="90px"
+          className="p-2 m-3 filter-invert" 
+          src="./src/res/cl_logo.png" 
+          title="Champions League" 
+        />
+        Fantasy
+      </NavLink>
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded && <FontAwesomeIcon color="white" icon={faTimes} />}
+        {!expanded && <FontAwesomeIcon color="white" icon={faBars} />}
+      </Navbar.Toggle>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav
+          onClick={() => {
+            if (expanded) setExpanded(!expanded);
+          }}
+        >
+          <NavLink className="nav-item nav-links" to="/">
+            Guide
           </NavLink>
-
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink
-                to="/"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/about"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/blog"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Blog
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/contact"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Contact Us
-              </NavLink>
-            </li>
-          </ul>
-          <div className="nav-icon" onClick={handleClick}>
-            {click && <FontAwesomeIcon icon={faTimes} />}
-            {!click && <FontAwesomeIcon icon={faBars} />}
-          </div>
-        </div>
-      </nav>
-    </>
+          <NavLink className="nav-item nav-links" to="/">
+            Lessons
+          </NavLink>
+          <NavLink className="nav-item nav-links" to="/">
+            Hotkeys
+          </NavLink>
+          <NavLink className="nav-item nav-links" to="/">
+            Autopilots
+          </NavLink>
+          <NavLink className="nav-item nav-links" to="/">
+            Learn Hotkeys
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
