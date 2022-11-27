dc9890f66c9c227d288adb4b916931135ac6ba80
Shalom Prinz <shalom.prinz@gmail.com>
2022-11-27 02:07:42 +0200

    add jersey for more teams, including default error jersey. getJersey function relies only on first word of given jersey

--- src/res/jersey/index.ts
+++ src/res/jersey/index.ts
@@ -1,13 +1,33 @@
 import { get } from "lodash";
 
 import Barcelona from "./Barcelona.png";
+import Chelsea from "./Chelsea.png";
 import Dortmund from "./Dortmund.png";
+import Error from "./Error.png";
+import Inter from "./Inter.png";
+import Liverpool from "./Liverpool.png";
+import Manchester from "./ManchesterCity.png";
+import Milan from "./Milan.png";
+import Napoli from "./Napoli.png";
+import Paris from "./Paris.png";
+import Real from "./RealMadrid.png";
+import Tottenham from "./Tottenham.png";
 
 const jerseys = {
   Barcelona,
   Dortmund,
+  Inter,
+  Liverpool,
+  Manchester,
+  Milan,
+  Chelsea,
+  Napoli,
+  Tottenham,
+  Real,
+  Paris,
 };
 
 export type Jersey = keyof typeof jerseys;
 
-export const getJersey = (jersey: Jersey) => get(jerseys, jersey, Barcelona);
+export const getJersey = (jersey: Jersey) =>
+  get(jerseys, jersey.split(" ")[0], Error);
